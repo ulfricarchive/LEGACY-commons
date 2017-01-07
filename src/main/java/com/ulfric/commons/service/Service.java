8a8808c4ae3d3a74794c83ee0aeb628d40881f94
Adam Harrison <ad@mharrison.us>
2017-01-07 17:53:50 -0500

    Improve Service#getName

--- src/main/java/com/ulfric/commons/service/Service.java
+++ src/main/java/com/ulfric/commons/service/Service.java
@@ -4,6 +4,18 @@ import com.ulfric.commons.naming.Named;
 
 public interface Service extends Named {
 
-	
+	@Override
+	default String getName()
+	{
+		return Named.tryToGetNameFromAnnotation(this).orElseGet(() ->
+		{
+			String name = this.getClass().getSimpleName();
+			if (name.endsWith("Service"))
+			{
+				name = name.substring(0, name.length() - "Service".length());
+			}
+			return name;
+		});
+	}
 
 }
