df7c893d30d15c9d29f31b17edf6313f26785433
Adam Harrison <ad@mharrison.us>
2017-04-02 05:38:48 -0400

    Named changes

--- src/main/java/com/ulfric/commons/naming/Named.java
+++ src/main/java/com/ulfric/commons/naming/Named.java
@@ -12,14 +12,30 @@ public interface Named {
 
 	static String getNameFromAnnotation(Named named)
 	{
-		return Named.tryToGetNameFromAnnotation(named).orElseThrow(NameMissingException::new);
+		Objects.requireNonNull(named);
+
+		return Named.getNameFromAnnotation(named.getClass());
 	}
 
 	static Optional<String> tryToGetNameFromAnnotation(Named named)
 	{
 		Objects.requireNonNull(named);
 
-		Name name = named.getClass().getAnnotation(Name.class);
+		return Named.tryToGetNameFromAnnotation(named.getClass());
+	}
+
+	static String getNameFromAnnotation(Class<? extends Named> named)
+	{
+		Objects.requireNonNull(named);
+
+		return Named.tryToGetNameFromAnnotation(named).orElseThrow(NameMissingException::new);
+	}
+
+	static Optional<String> tryToGetNameFromAnnotation(Class<? extends Named> named)
+	{
+		Objects.requireNonNull(named);
+
+		Name name = named.getAnnotation(Name.class);
 
 		return name == null ? Optional.empty() : Optional.of(name.value());
 	}
