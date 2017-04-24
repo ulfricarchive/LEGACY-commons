6dcb4c96c49a560d11a4ab983e78d5ef1127e7cb
Luke <development@inkzzz.com>
2017-04-24 23:16:25 +0100

    Numeric util

--- src/main/java/com/ulfric/commons/arithmetic/NumericUtils.java
+++ src/main/java/com/ulfric/commons/arithmetic/NumericUtils.java
@@ -13,4 +13,60 @@ public enum NumericUtils {
 		return NumericUtils.NUMERIC_STRIPPER.matcher(content).matches();
 	}
 	
+	public static boolean isInteger(String content)
+	{
+		try
+		{
+			Integer.parseInt(content);
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean isLong(String content)
+	{
+		try
+		{
+			Long.valueOf(content);
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean isFloat(String content)
+	{
+		try
+		{
+			Float.valueOf(content);
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public static boolean isDouble(String content)
+	{
+		try
+		{
+			Double.valueOf(content);
+		}
+		catch (NumberFormatException e)
+		{
+			return false;
+		}
+		
+		return true;
+	}
+	
 }
--- src/test/java/com/ulfric/commons/arithmetic/NumericUtilsTest.java
+++ src/test/java/com/ulfric/commons/arithmetic/NumericUtilsTest.java
@@ -12,31 +12,87 @@ import org.junit.runner.RunWith;
 public class NumericUtilsTest extends UtilTestBase {
 	
 	@Test
-	public void testIsNumericValid()
+	void testIsNumericValid()
 	{
 		String textual = "300";
 		Verify.that(NumericUtils.isNumeric(textual)).isTrue();
 	}
 	
 	@Test
-	public void testIsNumericInvalid()
+	void testIsNumericInvalid()
 	{
 		String textual = "aaa";
 		Verify.that(NumericUtils.isNumeric(textual)).isFalse();
 	}
 	
 	@Test
-	public void testIsNumericDecimalValid()
+	void testIsNumericDecimalValid()
 	{
 		String textual = "34.3";
 		Verify.that(NumericUtils.isNumeric(textual)).isTrue();
 	}
 	
 	@Test
-	public void testIsNumericDecimalInvalid()
+	void testIsNumericDecimalInvalid()
 	{
 		String textual = "a.dre";
 		Verify.that(NumericUtils.isNumeric(textual)).isFalse();
+	}
+	
+	@Test
+	void testIsIntegerValid()
+	{
+		String textual = "10000";
+		Verify.that(NumericUtils.isInteger(textual)).isTrue();
+	}
+	
+	@Test
+	void testIsIntegerInvalid()
+	{
+		String textual = "abd";
+		Verify.that(NumericUtils.isInteger(textual)).isFalse();
+	}
+	
+	@Test
+	void testIsLongValid()
+	{
+		String textual = "10000000000000";
+		Verify.that(NumericUtils.isLong(textual)).isTrue();
+	}
+	
+	@Test
+	void testIsLongInvalid()
+	{
+		String textual = "adf";
+		Verify.that(NumericUtils.isLong(textual)).isFalse();
+	}
+	
+	@Test
+	void testIsFloatValid()
+	{
+		String textual = "0.1";
+		Verify.that(NumericUtils.isFloat(textual)).isTrue();
+	}
+	
+	@Test
+	void testIsFloatInvalid()
+	{
+		String textual = "a.bd";
+		Verify.that(NumericUtils.isFloat(textual)).isFalse();
+	}
+	
+	@Test
+	void testIsDoubleValid()
+	{
+		String textual = "10.0";
+		Verify.that(NumericUtils.isDouble(textual)).isTrue();
+	}
+	
+	@Test
+	void testIsDoubleInvalid()
+	{
+		String textual = "ad.f";
+		Verify.that(NumericUtils.isDouble(textual)).isFalse();
 	}
 	
 }
